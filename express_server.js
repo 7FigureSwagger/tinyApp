292e6f4e6865b7213cab583b3c27559abdcd76e7
7 <swistek.labs@gmail.com>
2019-12-10 06:09:11 +0000

    part1 conclusion

--- express_server.js
+++ express_server.js
@@ -4,11 +4,17 @@ const PORT = 8080; //default port
 
 app.set('view engine', 'ejs');
 
+function generateRandomString() {
+
+}
+
 const urlDatabase = {
   'b2xVn2': 'http://www.lighthouselabs.ca',
   '9sm5xK': 'http://www.google.com'
 };
 
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get('/', (req, res) => {
   res.send('Hello!');
@@ -19,8 +25,17 @@ app.get('/urls', (req, res) => {
   res.render('urls_index', templateVars);
 });
 
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
+
 app.get('/urls.json', (req, res) => {
   res.json(urlDatabase);
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
 });
 
 app.get("/urls/:shortURL", (req, res) => {
@@ -35,6 +50,7 @@ app.get('/hello', (req, res) => {
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
+
 
 
 
